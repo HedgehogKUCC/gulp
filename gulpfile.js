bd2948ac4c248f25c7f244f6660da95580886f06
HedgehogKU <ccc908925@gmail.com>
2019-11-16 14:57:41 +0800

    gulp-postcss & autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var jade = require('gulp-jade');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('copyHTML', function () {
   return gulp.src('./source/**/*.html')
@@ -19,9 +21,19 @@ gulp.task('jade', function() {
 });
 
 gulp.task('sass', function () {
+
+  // var plugins = [
+  //   autoprefixer({ overrideBrowserslist: [ 'last 3 version', '> 5%', 'ie 6-8' ] })
+  // ];
+
   return gulp.src('./source/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
+    // 編譯完成 CSS
+
+    // .pipe(postcss(plugins))
+    .pipe(postcss( [autoprefixer()] ))  // 直接引入 autoprefixer
+    
     .pipe(gulp.dest('./public/css/'));
 });
 
