b60f1ee306f35c1f62bbc17f5f01cf8fc6002373
HedgehogKU <ccc908925@gmail.com>
2019-11-16 10:55:03 +0800

    gulp-jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp');
+var jade = require('gulp-jade');
 
 gulp.task('copyHTML', function () {
   return gulp.src('./source/**/*.html')
     .pipe(gulp.dest('./public/'))
-})
+});
+
+gulp.task('jade', function() {
+  // var YOUR_LOCALS = {};
+ 
+  return gulp.src('./source/**/*.jade')
+    .pipe(jade({
+      // locals: YOUR_LOCALS
+      pretty: true
+    }))
+    .pipe(gulp.dest('./public/'))
+});
