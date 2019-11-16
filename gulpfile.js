4d1cf78aa3cc405448ae67b21265df0ee3d66b1b
HedgehogKU <ccc908925@gmail.com>
2019-11-16 13:51:05 +0800

    gulp-sass & watch & plumber | gulp v4 : gulp-series & gulp-parallel

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
 
 gulp.task('copyHTML', function () {
   return gulp.src('./source/**/*.html')
@@ -7,12 +9,27 @@ gulp.task('copyHTML', function () {
 });
 
 gulp.task('jade', function() {
-  // var YOUR_LOCALS = {};
  
   return gulp.src('./source/**/*.jade')
+    .pipe(plumber())
     .pipe(jade({
-      // locals: YOUR_LOCALS
       pretty: true
     }))
     .pipe(gulp.dest('./public/'))
 });
+
+gulp.task('sass', function () {
+  return gulp.src('./source/scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./source/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./source/**/*.jade', gulp.series('jade'));
+});
+
+gulp.task('default', 
+  gulp.parallel('jade', 'sass', 'watch')
+);
